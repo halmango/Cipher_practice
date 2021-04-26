7bef5413998699e742ede08d3788555459f08dbd
halmango <71398115+halmango@users.noreply.github.com>
2021-04-26 23:11:35 +0900

    Update wordbookapp.py

--- Crawling/wordbookapp.py
+++ Crawling/wordbookapp.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
+from selenium import webdriver # webdriver 는 해당 파일과 같은 디렉토리에 위치해야 함. 오류가 난다면 구글 버전을 확인한다.
 import time
-import csv
+import csv # csv형태로 파일을 저장 할 예정
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -27,30 +27,23 @@ while True:
         print('이미 번역한 영단어입니다! 뜻은\n', My_wordbook[word], '입니다.')
     else:
         driver.find_element_by_xpath('//*[@id="q"]').send_keys(word) 
-        driver.find_element_by_css_selector('button#daumBtnSearch').click()
+        driver.find_element_by_css_selector('button#daumBtnSearch').click() # 검색창에 단어를 입력하고 검색 버튼 누르기
         try:
-            driver.find_element_by_xpath('//*[@id="mSub"]/div/ul')
+            driver.find_element_by_xpath('//*[@id="mSub"]/div/ul') 
             time.sleep(1)
-            meaning = driver.find_element_by_xpath('//*[@id="mSub"]/div/ul').text 
+            meaning = driver.find_element_by_xpath('//*[@id="mSub"]/div/ul').text # 검색 결과에서 따온 뜻을 text 형태로 변환
         except:
-            driver.find_element_by_xpath('//*[@id="mArticle"]/div[1]/div[2]/div[2]/div[1]/ul')
+            driver.find_element_by_xpath('//*[@id="mArticle"]/div[1]/div[2]/div[2]/div[1]/ul') # 몇 개의 단어는 오류가 발생. 이 코드로 단어 검색 결과를 가져온다.
             time.sleep(1)
-            meaning = driver.find_element_by_xpath('//*[@id="mArticle"]/div[1]/div[2]/div[2]/div[1]/ul').text
-            try:
-                driver.find_element_by_xpath('//*[@id="mArticle"]/div[1]/div[3]/div[2]/div/ul')
-                time.sleep(1)
-                meaning = driver.find_element_by_xpath('//*[@id="mArticle"]/div[1]/div[3]/div[2]/div/ul').text
-            except:
-
+            meaning = driver.find_element_by_xpath('//*[@id="mArticle"]/div[1]/div[2]/div[2]/div[1]/ul').text 
                     
-                print(meaning)
+        print(meaning) # 검색한 단어의 뜻을 출력
 
         wtr.writerow([word, meaning])
-        My_wordbook[word] = meaning
+        My_wordbook[word] = meaning # csv파일에 단어 -  뜻
 
-        driver.find_element_by_xpath('//*[@id="q"]').clear()
+        driver.find_element_by_xpath('//*[@id="q"]').clear() # 검색 창에 남아있는 이전에 검색한 단어 지우기
         time.sleep(1)
 
 driver.close()
 f.close()
-
